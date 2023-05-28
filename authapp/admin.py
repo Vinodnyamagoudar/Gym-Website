086aa0fe8d1680c21d5a2cca0bfbc56969f6070b
vinod <vinodnyamagoudar@gmail.com>
2023-05-29 02:13:12 +0530

    first commit

---
+++ authapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from authapp.models import Contact,MembershipPlan,Enrollment,Trainer,Attendance,Gallery
+
+
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
+admin.site.register(Gallery)
+admin.site.register(Attendance)
